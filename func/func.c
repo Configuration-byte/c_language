15bb01b561a4c5f062e8ad8305689b45340f7b89
Configuration-byte <948054294@qq.com>
2021-05-22 12:35:33 +0800

    [Curtis] 函数地址传递

--- func/func.c
+++ func/func.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if 0
 int print_value(int a, int b)
 {
 	printf("a = %d b = %d\n", a, b);
 	return 0;
+}
+#endif
+
+void swap(int *p1, int *p2)
+{
+	int tmp;
+
+	tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
 }
 
 int main(void)
 {
 	int i = 3, j = 5;
 
-	print_value(i, j);
+	swap(&i, &j);
+
+	printf("i = %d j = %d\n", i, j);
 
 	return 0;
 }
