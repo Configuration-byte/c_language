97cefbaeec8e77b4a8b40439e1435ca38eaaece6
Configuration-byte <948054294@qq.com>
2021-05-22 15:47:08 +0800

    [Curtis] 函数递归求阶乘

---
+++ func/func1.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int func(int n)
+{
+	if(n < 0)
+		return -1;
+
+	if(n == 0 || n == 1)
+		return 1;
+
+	return n * func(n -1);
+}
+
+int main(void)
+{
+	int n;
+	int res;
+
+	scanf("%d", n);
+
+	res = func(n);
+
+	printf("%d! = %d\n", n, res);
+
+	return 0;
+}
