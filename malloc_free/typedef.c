b3ffa13bd1f80f6235c4fc0c0a8cb3043de35c0c
Configuration-byte <948054294@qq.com>
2021-05-26 07:21:00 +0800

    [Curtis] typedef基本用法

---
+++ malloc_free/typedef.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * #define INT int
+ * typedef int INT;
+ *
+ * INT i;   -->   int i;
+ *
+ *
+ * #define IP int *
+ * typedef int *IP;
+ *
+ * IP p,q;  -->   int *p,q;
+ * IP p,q;  -->   int *p,*q;
+ *
+ * typedef int ARR[6];  -->   int [6] -> ARR;
+ *
+ * ARR a;   -->   int a[6];
+ *
+ * struct node_st 
+ * {
+ * 	int i;
+ * 	float f;
+ * };
+ *
+ * typedef struct node_st NODE;
+ * NODE a;  -> struct node_st a;
+ * NODE *p;  -> struct node_st *p;
+ *
+ * typedef struct node_st *NODEP;
+ * NODE p;  -> struct node_st *p;
+ *
+ * typedef struct 
+ * {
+ * 	int i;
+ * 	float f;
+ * }NODE,*NODEP;
+ *
+ * typedef int FUNC(int);  -> int(int) -> FUNC;
+ * FUNC f;  --> int f(int);
+ *
+ * typedef int *FUNCP(int);
+ * FUNCP p;  --> int *p(int);
+ *
+ *
+ * typedef int *(*FUNCP)(int);
+ * FUNCP p;   --> int *(*p)(int);
+ * */
+
+typedef int INT;
+
+int main(void)
+{
+	INT i = 100;	
+
+	printf("%d\n", i);
+
+
+	return 0;
+}
